72f790ecda00ba5c3cd7c16fdb05d131d8989658
TranTrongNghia3008 <nghia.trantrong3008@gmail.com>
2024-01-24 01:50:57 +0700

    update newsfeed_screen

--- lib/pages/newsfeed_screen.dart
+++ lib/pages/newsfeed_screen.dart
@@ -1,6 +1,10 @@
+import 'package:fluentui_system_icons/fluentui_system_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:fooderapp/pages/add_food_screen.dart';
 
 class NewsFeedScreen extends StatefulWidget {
+  const NewsFeedScreen({super.key});
+
   @override
   _NewsFeedScreenState createState() => _NewsFeedScreenState();
 }
@@ -9,57 +13,139 @@ class _NewsFeedScreenState extends State<NewsFeedScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       appBar: AppBar(
-        backgroundColor: Colors.black,
-        title: Text('Foodfeeds'),
+        // backgroundColor: Colors.black,
+        title: const Text(
+          'Foodfeeds',
+          style: TextStyle(
+            fontSize: 25.0,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
       ),
       body: ListView.builder(
         itemCount: 10, // This should be the length of your data list
         itemBuilder: (context, index) {
-          return NewsFeedItem(); // Custom widget for newsfeed item
+          return const NewsFeedItem(); // Custom widget for newsfeed item
         },
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          // Add your logic to navigate to the new post creation screen
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const AddFoodScreen(),
+            ),
+          );
+        },
+        backgroundColor: Colors.blue,
+        child: const Icon(Icons.add),
       ),
     );
   }
 }
 
 class NewsFeedItem extends StatelessWidget {
+  const NewsFeedItem({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Card(
-      color: Colors.black,
+      color: Colors.grey[900], // Dark background color
+      margin: const EdgeInsets.all(8.0),
+      elevation: 4.0,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12.0),
+      ),
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           ListTile(
-            leading: CircleAvatar(
-              // Profile picture
+            contentPadding: const EdgeInsets.all(8.0),
+            leading: const CircleAvatar(
+              // Placeholder image or actual image from data
+              backgroundImage: AssetImage('assets/images/jcoleFHD.jpg'),
             ),
-            title: Text('User Name'), // User name
-            subtitle: Text('Time ago'), // Post time
+            title: const Text(
+              'User Name',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 18.0,
+                color: Colors.white,
+              ),
+            ),
+            subtitle: const Text(
+              'Time ago',
+              style: TextStyle(
+                fontSize: 14.0,
+                color: Colors.grey,
+              ),
+            ),
+            trailing: PopupMenuButton(
+              icon: const Icon(
+                Icons.more_horiz,
+                color: Colors.white,
+              ),
+              itemBuilder: (context) => [
+                const PopupMenuItem(
+                  value: 'hide',
+                  child: Text('Hide'),
+                ),
+                const PopupMenuItem(
+                  value: 'report',
+                  child: Text('Report'),
+                ),
+                // Add more items as needed
+              ],
+              onSelected: (value) {
+                // Handle menu item selection
+                if (value == 'hide') {
+                  // Handle hide action
+                } else if (value == 'report') {
+                  // Handle report action
+                }
+              },
+            ),
           ),
-          Padding(
+          const Padding(
             padding: EdgeInsets.all(8.0),
-            child: Text('Post content goes here...'), // Post content
+            child: Text(
+              'Post content goes here...',
+              style: TextStyle(
+                fontSize: 16.0,
+                color: Colors.white,
+              ),
+            ),
           ),
-          Image.asset(
-            'assets/images/jcoleFHD.jpg', // Post image
-            fit: BoxFit.cover,
+          ClipRRect(
+            borderRadius: BorderRadius.circular(8.0),
+            child: Image.asset(
+              'assets/images/jcoleFHD.jpg',
+              fit: BoxFit.cover,
+              // height: 300.0, // Adjust the height as needed
+            ),
           ),
           ButtonBar(
+            alignment: MainAxisAlignment.spaceAround,
             children: <Widget>[
-              TextButton(
-                child: Icon(Icons.thumb_up),
-                onPressed: () {/* Like action */},
+              ElevatedButton.icon(
+                onPressed: () {},
+                icon:
+                    // post.isLiked?
+                    const Icon(FluentIcons.heart_20_filled),
+                // : const Icon(FluentIcons.heart_20_regular),
+                label: const Text('Like'),
               ),
-              TextButton(
-                child: Icon(Icons.comment),
+              ElevatedButton.icon(
                 onPressed: () {/* Comment action */},
+                icon: const Icon(Icons.comment),
+                label: const Text('Comment'),
               ),
-              TextButton(
-                child: Icon(Icons.share),
+              ElevatedButton.icon(
                 onPressed: () {/* Share action */},
+                icon: const Icon(Icons.share),
+                label: const Text('Share'),
               ),
             ],
           )
@@ -68,3 +154,51 @@ class NewsFeedItem extends StatelessWidget {
     );
   }
 }
+
+
+// class NewsFeedItem extends StatelessWidget {
+//   const NewsFeedItem({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Card(
+//       color: Colors.black,
+//       child: Column(
+//         crossAxisAlignment: CrossAxisAlignment.start,
+//         children: <Widget>[
+//           const ListTile(
+//             leading: CircleAvatar(
+//                 // Profile picture
+//                 ),
+//             title: Text('User Name'), // User name
+//             subtitle: Text('Time ago'), // Post time
+//           ),
+//           const Padding(
+//             padding: EdgeInsets.all(8.0),
+//             child: Text('Post content goes here...'), // Post content
+//           ),
+//           Image.asset(
+//             'assets/images/jcoleFHD.jpg', // Post image
+//             fit: BoxFit.cover,
+//           ),
+//           ButtonBar(
+//             children: <Widget>[
+//               TextButton(
+//                 child: const Icon(Icons.thumb_up),
+//                 onPressed: () {/* Like action */},
+//               ),
+//               TextButton(
+//                 child: const Icon(Icons.comment),
+//                 onPressed: () {/* Comment action */},
+//               ),
+//               TextButton(
+//                 child: const Icon(Icons.share),
+//                 onPressed: () {/* Share action */},
+//               ),
+//             ],
+//           )
+//         ],
+//       ),
+//     );
+//   }
+// }
