bc4bf91f124a2591c52b057302860b25a186c12a
TranTrongNghia3008 <nghia.trantrong3008@gmail.com>
2024-01-23 20:22:22 +0700

    upload add_food_screen

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:fooderapp/pages/detail_dish_screen.dart';
 import 'package:fooderapp/pages/home_page_content_screen.dart';
 import 'package:fooderapp/pages/newsfeed_screen.dart';
 import 'package:fooderapp/pages/search_screen.dart';
+import 'package:fooderapp/pages/add_food_screen.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -15,20 +16,20 @@ class _HomePageState extends State<HomePage> {
   int _currentNavItem = 0;
 
   final List<Widget> _screens = [
-    HomePageContent(),
-    NewsFeedScreen(), 
+    const HomePageContent(),
+    NewsFeedScreen(),
     SearchScreen(),
     //DetailsDishScreen(),
+    const AddFoodScreen(),
   ];
-
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(  
+    return Scaffold(
       bottomNavigationBar: BottomNavigationBar(
         type: BottomNavigationBarType.fixed,
         currentIndex: _currentNavItem,
-        items: const [                        
+        items: const [
           BottomNavigationBarItem(
               activeIcon: Icon(Icons.home),
               icon: Icon(Icons.home_outlined),
@@ -45,8 +46,6 @@ class _HomePageState extends State<HomePage> {
               icon: Icon(Icons.library_music_outlined),
               label: 'Library',
               activeIcon: Icon(Icons.library_music_rounded)),
-          
-              
         ],
         fixedColor: Colors.white,
         iconSize: 20,
